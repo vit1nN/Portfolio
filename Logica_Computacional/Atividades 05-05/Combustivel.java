42d125e131c62b7d1e73b74b64a9c2c737a40b4a
Vitor dos Santos Pires <102592994+vit1nN@users.noreply.github.com>
2022-05-17 07:52:44 -0300

    Update Combustivel.java

--- Logica_Computacional/Atividades 05-05/Combustivel.java
+++ Logica_Computacional/Atividades 05-05/Combustivel.java
@@ -5,12 +5,12 @@ import javax.swing.JOptionPane;
 public class Combustivel {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Bom dia, Boa Tarde, Boa Noite!");
-        JOptionPane.showMessageDialog(null, "Seja bem vindo ao sistema de calculo com gastos em combustível.");
+        JOptionPane.showMessageDialog(null, "Seja bem vindo ao sistema de cálculo com gastos em combustível.");
         String km = JOptionPane.showInputDialog("Qual a distância a ser percorrida pelo veículo? (em km)");
         int a = Integer.parseInt(km);
         String kmlitro = JOptionPane.showInputDialog("Qual o consumo do veículo em km/litro?");
         int b = Integer.parseInt(kmlitro);
-        String preço = JOptionPane.showInputDialog("Por fim, qual o valor do preço do Litro do combustível?");
+        String preço = JOptionPane.showInputDialog("Por fim, qual o valor do preço do Litro do combustível? (Use ponto ao invés de vírgula)");
         double c = Double.parseDouble(preço);
         calculo(a, b, c);
     }
